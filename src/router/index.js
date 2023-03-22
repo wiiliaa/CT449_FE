770f2086e8e03158f22dd4445d42fcecb3978b55
datduong <datduong010@gmail.com>
2023-03-22 19:53:48 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,14 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 import router from "./router";
 createApp(App).use(router).mount("#app");
-const routes = [{ path: "/", name: "contactbook", component: ContactBook }];
+const routes = [
+  { path: "/", name: "contactbook", component: ContactBook },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
