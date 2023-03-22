5a9bf38bca46383b7ff8943b7c12ee73f893371a
datduong <datduong010@gmail.com>
2023-03-22 19:47:02 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+import router from "./router";
+createApp(App).use(router).mount("#app");
+const routes = [{ path: "/", name: "contactbook", component: ContactBook }];
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+export default router;
--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,8 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  server: { port: 3001 },
+  server: {
+    port: 3001,
+    proxy: { "/api": { target: "http://localhost:3000/", changeOrigin: true } },
+  },
 });
